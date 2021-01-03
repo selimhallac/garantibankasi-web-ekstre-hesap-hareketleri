bf7de9992e9b30192f2124cfb19551a74070cc6f
PHP.dev <php.dev@PHPdev.local>
2021-01-03 22:45:00 +0300

    Garanti Bankası Hesap Hareketleri

---
+++ ekstre.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Class ZiraatBank
+@author Selim Hallaç
+@blog selimhallac.com
+*/
+
+require 'garanti.class.php';
+
+$unvan = ""; // Banka tarafından verilen unvan bölümü
+$kurum_kodu = ""; // Banka tarafından verilen kurum kodu
+$key = ""; // Garanti bankası size bir exe verecek. O exeyi bilgisayarınızda açıp ip adresini yazıp bir key üreteceksiniz. O keyi buraya yazınız. Eğer siz üretemiyorsanız onlara mail atın onlar vereceklerdir.
+
+$entegrasyon = new Garanti($unvan,$kurum_kodu,$key);
+
+$response = $entegrasyon->hesap_hareketleri('2020-12-25');
+
+echo '<pre>';
+print_r($response);
+echo '</pre>';
---
+++ garanti.class.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Class Garanti
+@author Selim Hallaç
+@blog selimhallac.com
+*/
+
+class Garanti {
+  public $kurumKodu = "";
+  public $parola = "";
+  public $token = "";
+  public $url = "https://inboundrstintws.garanti.com.tr/services/FirmAccountActivitySoap";
+  function __construct($kurumkodu,$parola,$key)
+  {
+    $this->$kurumKodu = $kurumkodu;
+    $this->parola = $parola;
+    $this->token = $key;
+  }
+
+  public function hesap_hareketleri($tarih){
+    $data = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:odem="http://odemeler.garanti.com.tr/">
+	   <soapenv:Header/>
+	   <soapenv:Body>
+		  <odem:FirmAccountActivity>
+			 <odem:securetoken>
+				<odem:UserId>'.$this->kurumKodu.'</odem:UserId>
+				<odem:Password></odem:Password>
+				<odem:CreateTimestamp></odem:CreateTimestamp>
+				<odem:Encoded>'.$this->token.'</odem:Encoded>
+			 </odem:securetoken>
+			 <odem:FirmCode>'.$this->parola.'</odem:FirmCode>
+			 <odem:StartDate>'.$tarih.'-00.00.00.000001</odem:StartDate>
+			 <odem:EndDate>'.$tarih.'-23.59.59.999999</odem:EndDate>
+			 <odem:BranchNum></odem:BranchNum>
+			 <odem:AccountNum></odem:AccountNum>
+			 <!--Optional:-->
+			 <odem:IBAN></odem:IBAN>
+			 <odem:TransactionId></odem:TransactionId>
+		  </odem:FirmAccountActivity>
+	   </soapenv:Body>
+	    </soapenv:Envelope>';
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml', 'SOAPAction:add'));
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      $response = curl_exec($ch);
+      curl_close($ch);
+      return $output;
+  }
+
+
+
+}
