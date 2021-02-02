ccd7494d9ec41c62da6bde4faa71befd4c229379
PHP.dev <php.dev@PHPdev.local>
2021-02-02 15:10:44 +0300

    Update ekstre.php

--- ekstre.php
+++ ekstre.php
@@ -10,8 +10,8 @@ require 'garanti.class.php';
 $unvan = ""; // Banka tarafından verilen unvan bölümü
 $kurum_kodu = ""; // Banka tarafından verilen kurum kodu
 $key = ""; // Garanti bankası size bir exe verecek. O exeyi bilgisayarınızda açıp ip adresini yazıp bir key üreteceksiniz. O keyi buraya yazınız. Eğer siz üretemiyorsanız onlara mail atın onlar vereceklerdir.
-
-$entegrasyon = new Garanti($unvan,$kurum_kodu,$key);
+$IBAN = "";
+$entegrasyon = new Garanti($unvan,$kurum_kodu,$key,$IBAN);
 
 $response = $entegrasyon->hesap_hareketleri('2020-12-25');
 
